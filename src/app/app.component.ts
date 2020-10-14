8d5aec6cd2a7bd22a66aba6c56e40f9419cf84db
murukai.gumbo <murukai.gumbo@playsafesa.com>
2020-10-14 12:57:33 +0200

    Commit after finishing first chapter reading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-reddit';
+
+  articles: Article[];
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean{
+    console.log(`Starting to add article with values : { ${title.value} , ${link.value} }`)
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
+    console.log(`Added article : ${title.value} and link : ${link.value}`)
+    return false;
+  }
+
+  constructor(){
+    this.articles = [
+      new Article('Africatek','http://angular.io',5),
+      new Article('github','http://github.com',3),
+      new Article('gitlab','http://gitlab.io',1)
+    ];
+  }
+
+  sortedArticles(): Article[]{
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
 }
